14c9dfd7fc5f7e5296016b9c37279fd47ab48d50
matleoudemm <leonel.mateo@alumnos.udemm.edu.ar>
2019-08-29 00:05:01 -0300

    Adicion modulo Yin

--- Main/comp.sh
+++ Main/comp.sh
@@ -9,7 +9,7 @@ salida="vibrotact"
 entrada1="main.c"
 entrada2="kiss_fft.c"
 entrada3="filtros.c"
-#entrada4="otras_funciones.c"
+entrada4="Yin.c"
 entrada5="selector.c"
 
 echo -e "Modo debug [S/N]?"
@@ -20,7 +20,7 @@ then
    debug="-DDEBUG"
 fi
 
-gcc -O3 $entrada1 $entrada2 $entrada3 $entrada5 -Wall -o $salida $debug -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
+gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 -Wall -o $salida $debug -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
 #gcc $entrada1 $entrada2 $entrada3 $entrada5 -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
 
 echo "Programa compilado - nombre del ejecutable: " $salida
