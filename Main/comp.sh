45e267180b8fc55528868168738c116a2bf3a83a
matleoudemm <leonel.mateo@alumnos.udemm.edu.ar>
2019-09-08 15:00:17 -0300

    Incluir cambios del main y debug

--- Main/comp.sh
+++ Main/comp.sh
@@ -8,7 +8,7 @@ salida="vibrotact"
 
 entrada1="main.c"
 entrada2="kiss_fftr.c"
-entrada3="filtros.c"
+entrada3="fft.c"
 entrada4="Yin.c"
 entrada5="selector.c"
 entrada6="kiss_fft.c"
@@ -21,7 +21,8 @@ then
    debug="-DDEBUG"
 fi
 
-gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 -Wall -o $salida $debug -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
+gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
+#gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 $debug -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
 #gcc $entrada1 $entrada2 $entrada3 $entrada5 -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
 
 echo "Programa compilado - nombre del ejecutable: " $salida
