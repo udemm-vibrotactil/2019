4140e8990f43cf63ff46770ec39f4bbb82cc426b
matleoudemm <leonel.mateo@alumnos.udemm.edu.ar>
2019-09-12 01:03:29 -0300

    Correccion del modo debug

--- Main/comp.sh
+++ Main/comp.sh
@@ -13,16 +13,21 @@ entrada4="Yin.c"
 entrada5="selector.c"
 entrada6="kiss_fft.c"
 
-echo -e "Modo debug [S/N]?"
-read modo_debug
+#echo -e "Modo debug [S/N]?"
+read -p "Modo debug [S/N]?:" modo_debug
 
-if [ modo_debug="S" ]
-then
-   debug="-DDEBUG"
+if [ $modo_debug == "S" ]; then
+	debug1="-DDEBUG"
+elif [ $modo_debug == "N" ]; then
+	debug1=""
+else
+	echo "Error input"
+	exit
 fi
 
-gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
-#gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 $debug -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
+#gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
+gcc -O3 $entrada1 $entrada2 $entrada3 $entrada4 $entrada5 $entrada6 $debug1 -Wall -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp 
+#-ffast-math
 #gcc $entrada1 $entrada2 $entrada3 $entrada5 -o $salida -lpulse -lstdc++ -lpulse-simple -lm -fopenmp
 
 echo "Programa compilado - nombre del ejecutable: " $salida
