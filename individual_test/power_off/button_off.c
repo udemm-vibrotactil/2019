5fc1791c4bbea16b661713ecba5718a1201ce997
matleoudemm <leonel.mateo@alumnos.udemm.edu.ar>
2019-09-24 01:47:14 -0300

    Ajustes de interrupcion de apagado

--- individual_test/power_off/button_off.c
+++ individual_test/power_off/button_off.c
@@ -4,8 +4,8 @@
  
 void myInterrupt (void) 
 { 
-//system("reboot");
-system("halt");
+system("shutdown -P now");
+//system("poweroff -f");
 }
  
 int main (void)
@@ -14,7 +14,7 @@ int main (void)
   pinMode (6, INPUT) ;           // PIN 26 als Ausgang konfigurieren
   pullUpDnControl (6, PUD_UP) ;  // PIN 26 mit Pul-Upp versehen
   wiringPiISR (6, INT_EDGE_FALLING, &myInterrupt) ; // Interrupt Routine definieren
-  while(1) delay (5000) ;                       // Endlosschleife
+  while(1) delay (500) ;                       // Endlosschleife
   return 0 ;
 }
 
